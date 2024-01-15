9d1c308e6ff39100e60d32bfc3151c81e4840110
takutouehara <138412475+takutouehara@users.noreply.github.com>
2024-01-15 14:56:40 +0900

    コード入力
    01/15

--- Game/DriveAndAvoid/Object/Enemy.h
+++ Game/DriveAndAvoid/Object/Enemy.h
@@ -1,1 +1,26 @@
 #pragma once
+
+#include "../Utility/Vector2D.h"
+
+class Enemy
+{
+private:
+	int type;			// タイプ
+	int image;			// 画像
+	float speed;		// 速さ
+	Vector2D location;	// 位置情報
+	Vector2D box_size;	// 当たり判定の大きさ
+
+public:
+	Enemy(int type, int handle);
+	~Enemy();
+
+	void Initialize();
+	void Update(float speed);
+	void Draw()const;
+	void Finalize();
+	
+	int GetType() const;			// タイプ取得
+	Vector2D GetLocation()const;	// 位置情報の取得
+	Vector2D GetBoxSize()const;		// 当たり判定の大きさを取得
+};
