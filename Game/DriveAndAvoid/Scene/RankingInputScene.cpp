5f489fbd6ca319138f667cfe2c54f796413a9c76
takutouehara <138412475+takutouehara@users.noreply.github.com>
2024-01-18 12:14:12 +0900

    コードの入力
    01/18

---
+++ Game/DriveAndAvoid/Scene/RankingInputScene.cpp
@@ -0,0 +1,40 @@
+#include "RankingInputScene.h"
+#include "../Utility/InputControl.h"
+#include "DxLib.h"
+
+RankingInputScene::RankingInputScene()
+{
+
+}
+
+RankingInputScene::~RankingInputScene()
+{
+
+}
+
+void RankingInputScene::Initialize()
+{
+}
+
+eSceneType RankingInputScene::Update()
+{
+	return eSceneType();
+}
+
+void RankingInputScene::Draw() const
+{
+}
+
+void RankingInputScene::Finalize()
+{
+}
+
+eSceneType RankingInputScene::GetNowScene() const
+{
+	return eSceneType();
+}
+
+bool RankingInputScene::InputName()
+{
+	return false;
+}
