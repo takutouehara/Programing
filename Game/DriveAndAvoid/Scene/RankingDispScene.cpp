c2afb9a1dd4a5adaeb67e38e4306b8ace08dbb37
takutouehara <138412475+takutouehara@users.noreply.github.com>
2024-02-14 11:07:46 +0900

    uehara

--- Game/DriveAndAvoid/Scene/RankingDispScene.cpp
+++ Game/DriveAndAvoid/Scene/RankingDispScene.cpp
@@ -59,7 +59,24 @@ void RankingDispScene::Draw() const
 	for (int i = 0; i < 5; i++)
 	{
 		DrawFormatString(50, 170 + i * 100, 0xff0000, "%2d", ranking->GetRank(i));
-		DrawFormatString(200, 170 + i * 100, 0xffffff, "%-15s %6d", ranking->GetName(i), ranking->GetTime(i));
+		DrawFormatString(200, 170 + i * 100, 0xffffff, "%-15s", ranking->GetName(i));
+
+		if (ranking->GetTime(i) / 60 < 10 && ranking->GetTime(i) % 60 < 10)
+		{
+			DrawFormatString(800, 170 + i * 100, 0xffffff, "0%d:0%d", ranking->GetTime(i) / 60, ranking->GetTime(i) % 60);
+		}
+		if (ranking->GetTime(i) / 60 < 10 && ranking->GetTime(i) % 60 > 9)
+		{
+			DrawFormatString(800, 170 + i * 100, 0xffffff, "0%d:%d", ranking->GetTime(i) / 60, ranking->GetTime(i) % 60);
+		}
+		if (ranking->GetTime(i) / 60 > 9 && ranking->GetTime(i) % 60 < 10)
+		{
+			DrawFormatString(800, 170 + i * 100, 0xffffff, "%d:0%d", ranking->GetTime(i) / 60, ranking->GetTime(i) % 60);
+		}
+		if (ranking->GetTime(i) / 60 > 9 && ranking->GetTime(i) % 60 > 9)
+		{
+			DrawFormatString(800, 170 + i * 100, 0xffffff, "%d %d", ranking->GetTime(i) / 60, ranking->GetTime(i) % 60);
+		}
 	}
 }
 
